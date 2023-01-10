eee80518d3a1bd8f45d3ab836a4739353ca14d57
Natanael <natanaeldepaulo123@gmail.com>
2023-01-10 15:37:30 -0300

    feat:

--- backend/src/app/types/index.ts
+++ backend/src/app/types/index.ts
@@ -5,8 +5,25 @@ interface UserProps {
 }
 
 
+interface ClientProps {
+  name: string;
+  email: string;
+  telephone: string;
+  address: Address;
+  cpf: string;
+}
 
+type Address = {
+  street: string;
+  houseNumber: string;
+  district: string;
+  cep: string;
+  city: string;
+  state: string;
+}
 
 export {
-	UserProps
+	UserProps,
+	ClientProps,
+	Address
 };
