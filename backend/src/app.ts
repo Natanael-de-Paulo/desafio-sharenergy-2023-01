3a0a00fd0a53b6a574ac223635d0ee16a163020f
Natanael <natanaeldepaulo123@gmail.com>
2023-01-10 10:52:43 -0300

    feat: Connection with database mongoDB

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import CORS from 'cors';
 import helmet from 'helmet';
+import routes from './app/routes';
+import { ConnectDb } from './app/database';
 
 const app = express();
 
@@ -8,5 +10,11 @@ app.use(CORS());
 app.use(helmet());
 app.use(express.json());
 
+//Connection witch mongoDB 
+app.use((req, res, next) => ConnectDb
+	.then(() => next())
+	.catch(err => next(err)));
+  
+app.use('/v1', routes);
 
 export default app;
---
+++ backend/src/app/database/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', true);
+
+export const ConnectDb = mongoose.connect(
+	`${process.env.MONGO_URL || 'mongodb://localhost:27017/mydb'}_${
+		process.env.NODE_ENV || 'development'
+	}`,
+	// https://mongoosejs.com/docs/connections.html#options
+	{
+		serverSelectionTimeoutMS: !process.env.NODE_ENV ? 1000 : 30000,
+	} // Keep trying to send operations for 5 seconds
+);
+
+mongoose.connection.on('error', () => {
+	console.error('Mongo not connected');
+});
+mongoose.connection.on('connected', () => {
+	console.info('Mongo connected');
+});
+mongoose.connection.on('disconnected', () => {
+	console.error('Mongo disconnected');
+});
