6190840374082c8bd929e220fadf976907a49c0a
Natanael <natanaeldepaulo123@gmail.com>
2023-01-10 13:07:02 -0300

    feat: I implemented a method to log the user and generate an access token, I created a middleware to let only those who have a valid token use protected routes

--- backend/src/app/routes/Security.ts
+++ backend/src/app/routes/Security.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { authUser } from '../controllers/security/AuthUserController';
 import { createUser } from '../controllers/security/CreateUserController';
 
 export const SecurityRoutes = Router();
 
 SecurityRoutes
-	// .post('/login', authUserController.handle )
+	.post('/login', authUser )
 	.post('/register', createUser );
