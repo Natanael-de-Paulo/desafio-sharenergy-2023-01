216af96ace96127b0e95d3989231af6017eb6b55
Natanael <natanaeldepaulo123@gmail.com>
2023-01-10 16:44:47 -0300

    feat: method for delete client

--- backend/src/app/routes/Client.ts
+++ backend/src/app/routes/Client.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { createClient } from '../controllers/client/CreateClientController';
-import { ListClientById } from '../controllers/client/ListClientByIdController';
+import { deleteClient } from '../controllers/client/DeleteClientController';
+import { listClientById } from '../controllers/client/ListClientByIdController';
 import { listClients } from '../controllers/client/ListClientsController';
 
 export const ClientRoutes = Router();
 
 ClientRoutes
 	.get('/', listClients )
-	.get('/:client_id', ListClientById)
 	.post('/create', createClient );
+
+ClientRoutes
+	.route('/:client_id')
+	.get( listClientById )
+	.delete( deleteClient );
