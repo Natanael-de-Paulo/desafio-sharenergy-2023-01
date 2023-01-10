1ff9f0e7ec50a3688c170a0bf61c58124257f6b9
Natanael <natanaeldepaulo123@gmail.com>
2023-01-10 11:31:45 -0300

    feat: encrypting password

--- backend/src/app/services/security/CreateUserService.ts
+++ backend/src/app/services/security/CreateUserService.ts
@@ -1,14 +1,16 @@
-import { Response, Request } from 'express';
+import bcrypt from 'bcrypt';
 import { User } from '../../models/User';
 import { UserProps } from '../../types';
 
 class CreateUserService {
 	async execute({ user, password }: UserProps){
+		const passwordHash = await bcrypt.hash(password, 10);
+
 		const userCreated = await User.create({
 			user,
-			password
+			password: passwordHash
 		});
-		
+
 		return userCreated;
 	}
 }
