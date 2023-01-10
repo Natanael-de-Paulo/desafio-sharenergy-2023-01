ce3d7914a91fcb9f99dafd7d2ca9a016c49c4e77
Natanael <natanaeldepaulo123@gmail.com>
2023-01-10 16:29:03 -0300

    feat: method list client by id

--- backend/src/app/routes/Client.ts
+++ backend/src/app/routes/Client.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { createClient } from '../controllers/client/CreateClientController';
+import { ListClientById } from '../controllers/client/ListClientByIdController';
 import { listClients } from '../controllers/client/ListClientsController';
 
 export const ClientRoutes = Router();
 
 ClientRoutes
 	.get('/', listClients )
+	.get('/:client_id', ListClientById)
 	.post('/create', createClient );
